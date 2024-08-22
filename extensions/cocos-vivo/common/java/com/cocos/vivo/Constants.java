a18671b4754dcd15938182c8ba39e4463f7838fc
onerain <onerain88@gmail.com>
2024-08-22 17:34:22 +0800

    feat: 支持 Vivo 登录回调

--- extensions/cocos-vivo/common/java/com/cocos/vivo/Constants.java
+++ extensions/cocos-vivo/common/java/com/cocos/vivo/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 
     public static final String INIT = "ccams_init";
     public static final String EXIT_GAME = "ccams_exit_game";
+    public static final String LOGIN_SUCCESS = "ccams_login_success";
 }
--- extensions/cocos-vivo/common/java/com/cocos/vivo/VivoService.java
+++ extensions/cocos-vivo/common/java/com/cocos/vivo/VivoService.java
@@ -11,43 +11,52 @@ import com.vivo.unionsdk.open.VivoExitCallback;
 import com.vivo.unionsdk.open.VivoUnionSDK;
 
 public class VivoService implements SDKWrapper.SDKInterface {
-  private CocosActivity cocosActivity = null;
 
   @Override
   public void init(Context context) {
     Log.i(Constants.TAG, "VivoService init");
-    cocosActivity = (CocosActivity) context;
-    JsbBridgeWrapper.getInstance().addScriptEventListener(Constants.INIT, new JsbBridgeWrapper.OnScriptEventListener() {
-      @Override
-      public void onScriptEvent(String arg) {
-        Log.i(Constants.TAG, "Vivo SDK init");
-        VivoUnionSDK.onPrivacyAgreed(cocosActivity);
-        VivoUnionSDK.setDynamicShortcuts(cocosActivity, true, new DynamicShortcutsCallback() {
-          @Override
-          public void onDynamicShortcutsStatus(int status) {
-            // status = 1表示操作成功；其他错误值含义见文档
-            Log.d(Constants.TAG, "setDynamicShortcuts status: " + status);
-          }
-        });
-      }
-    });
-    JsbBridgeWrapper.getInstance().addScriptEventListener(Constants.EXIT_GAME,
-        new JsbBridgeWrapper.OnScriptEventListener() {
-          @Override
-          public void onScriptEvent(String arg) {
-            Log.i(Constants.TAG, "Vivo SDK exit game");
-            VivoUnionSDK.exit(cocosActivity, new VivoExitCallback() {
-              @Override
-              public void onExitCancel() {
-
-              }
-
-              @Override
-              public void onExitConfirm() {
-                android.os.Process.killProcess(android.os.Process.myPid());
-              }
-            });
-          }
-        });
+    JsbBridgeWrapper.getInstance().addScriptEventListener(Constants.INIT, initListener);
+    JsbBridgeWrapper.getInstance().addScriptEventListener(Constants.EXIT_GAME, exitGameListener);
   }
+
+  @Override
+  public void onDestroy() {
+    JsbBridgeWrapper.getInstance().removeScriptEventListener(Constants.INIT, initListener);
+    JsbBridgeWrapper.getInstance().removeScriptEventListener(Constants.EXIT_GAME, exitGameListener);
+  }
+
+  private final JsbBridgeWrapper.OnScriptEventListener initListener = new JsbBridgeWrapper.OnScriptEventListener() {
+    @Override
+    public void onScriptEvent(String arg) {
+      Log.i(Constants.TAG, "Vivo SDK init");
+      VivoUnionSDK.onPrivacyAgreed(SDKWrapper.shared().getActivity());
+      VivoUnionSDK.setDynamicShortcuts(SDKWrapper.shared().getActivity(), true, new DynamicShortcutsCallback() {
+        @Override
+        public void onDynamicShortcutsStatus(int status) {
+          // status = 1表示操作成功；其他错误值含义见文档
+          Log.d(Constants.TAG, "setDynamicShortcuts status: " + status);
+        }
+      });
+      // 注意：Vivo 在初始化后会自动登录账号，所以没有必要再登录 Vivo 账号
+      JsbBridgeWrapper.getInstance().dispatchEventToScript(Constants.LOGIN_SUCCESS);
+    }
+  };
+
+  private final JsbBridgeWrapper.OnScriptEventListener exitGameListener = new JsbBridgeWrapper.OnScriptEventListener() {
+    @Override
+    public void onScriptEvent(String arg) {
+      Log.i(Constants.TAG, "Vivo SDK exit game");
+      VivoUnionSDK.exit(SDKWrapper.shared().getActivity(), new VivoExitCallback() {
+        @Override
+        public void onExitCancel() {
+
+        }
+
+        @Override
+        public void onExitConfirm() {
+          android.os.Process.killProcess(android.os.Process.myPid());
+        }
+      });
+    }
+  };
 }
