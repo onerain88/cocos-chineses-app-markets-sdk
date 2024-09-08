78d1755f5f0035d0137af9e3a594815326d80a72
onerain <onerain88@gmail.com>
2024-09-08 22:36:34 +0800

    feat: 支持小米登录成功/失败回调

--- extensions/cocos-xiaomi/common/java/com/cocos/xiaomi/Constants.java
+++ extensions/cocos-xiaomi/common/java/com/cocos/xiaomi/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     public static final String TAG = "CocosXiaoMi";
     public static final String INIT = "ccams_init";
     public static final String EXIT_GAME = "ccams_exit_game";
+
+    public static final String LOGIN_SUCCESS = "ccams_login_success";
+    public static final String LOGIN_FAILURE = "ccams_login_failure";
 }
